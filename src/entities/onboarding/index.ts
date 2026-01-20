14d65ac3932acc9645614bbf817dcd04161c9904
Debuging-JunSeoPark <pjseo1313@daum.net>
2026-01-21 02:06:05 +0900

    chore: 직무 api타입 정의

--- src/entities/onboarding/index.ts
+++ src/entities/onboarding/index.ts
@@ -1,2 +1,3 @@
 export type { CareerLevel, CareerLevelsResponse } from './types/career';
+export type { Job, JobsResponse } from './types/job';
 export type { Skill, SkillsResponse } from './types/skill';
