2b9543e46e180c4313e1fe4cda814f4a19e82b31
Debuging-JunSeoPark <pjseo1313@daum.net>
2026-01-24 23:45:09 +0900

    feat: BFF 토큰 처리 로직 추가

--- src/features/auth/index.ts
+++ src/features/auth/index.ts
@@ -1,4 +1,4 @@
 export { default as KakaoLoginButton } from './ui/KakaoLoginButton';
-export { kakaoLogin } from './api/kakaoLogin';
 export type { KakaoOAuthLoginData } from '@/entities/auth';
 export { getKakaoAuthorizeUrl } from './api/kakaoAuthorize';
+export { kakaoLogin } from './api/kakaoLogin.client';
