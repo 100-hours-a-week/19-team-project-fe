534a5a67300cdf167e5d7ae139bf204bbe241a59
FE_juncci <81576126+Debuging-JunSeoPark@users.noreply.github.com>
2026-02-01 15:34:16 +0900

    [ISSUE - #126] URL GUARD 설정 (#127)
    * feat: url guard 설정
    
    * feat: url guard 토스트메시지 설정
    
    * docs: url guard 전략 문서화

--- src/widgets/home/index.ts
+++ src/widgets/home/index.ts
@@ -1,3 +1,4 @@
 export { default as SignupConfetti } from './ui/SignupConfetti';
 export { default as RecruitmentLinksTicker } from './ui/RecruitmentLinksTicker';
 export { default as TechBlogTicker } from './ui/TechBlogTicker';
+export { HomeGuardToast } from './ui/HomeGuardToast';
