dabfd5f334099da23282a7f1d07081b54ff143a5
Debuging-JunSeoPark <pjseo1313@daum.net>
2026-02-03 20:02:45 +0900

    feat: 현직자 구직자 가이드라인 추가

--- src/widgets/home/index.ts
+++ src/widgets/home/index.ts
@@ -2,3 +2,4 @@ export { default as SignupConfetti } from './ui/SignupConfetti';
 export { default as RecruitmentLinksTicker } from './ui/RecruitmentLinksTicker';
 export { TechBlogBanner, default as TechBlogTicker } from './ui/TechBlogTicker';
 export { HomeGuardToast } from './ui/HomeGuardToast';
+export { default as GuideButtons } from './ui/GuideButtons';
