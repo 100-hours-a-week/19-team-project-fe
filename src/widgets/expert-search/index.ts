5363f2eba1d2c3795c42c3cbca32f61cd0d88505
Debuging-JunSeoPark <pjseo1313@daum.net>
2026-02-06 19:11:40 +0900

    refactor: widgets-resume 폴더구조 정리

--- src/widgets/expert-search/index.ts
+++
@@ -1,1 +0,0 @@
-export { default as ExpertSearchPage } from './ui/ExpertSearchPage';
---
+++ src/widgets/expert/index.ts
@@ -0,0 +1,4 @@
+export { default as ExpertDetailPage } from './ui/ExpertDetailPage';
+export { default as ExpertDetailHeader } from './ui/ExpertDetailHeader';
+export { default as ExpertSearchPage } from './ui/ExpertSearchPage';
+export { default as ExpertSearchHeader } from './ui/ExpertSearchHeader';
