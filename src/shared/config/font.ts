5f4e2eda8ee07f16fab0a8f0a7f10fd135f9b492
Debuging-JunSeoPark <pjseo1313@daum.net>
2026-02-14 17:48:31 +0900

    refactor: 폰트 최적화

--- src/shared/config/font.ts
+++ src/shared/config/font.ts
@@ -3,21 +3,6 @@ import localFont from 'next/font/local';
 export const pretendard = localFont({
   src: [
     {
-      path: '../asset/fonts/Pretendard-Thin.woff2',
-      weight: '100',
-      style: 'normal',
-    },
-    {
-      path: '../asset/fonts/Pretendard-ExtraLight.woff2',
-      weight: '200',
-      style: 'normal',
-    },
-    {
-      path: '../asset/fonts/Pretendard-Light.woff2',
-      weight: '300',
-      style: 'normal',
-    },
-    {
       path: '../asset/fonts/Pretendard-Regular.woff2',
       weight: '400',
       style: 'normal',
@@ -35,16 +20,6 @@ export const pretendard = localFont({
     {
       path: '../asset/fonts/Pretendard-Bold.woff2',
       weight: '700',
-      style: 'normal',
-    },
-    {
-      path: '../asset/fonts/Pretendard-ExtraBold.woff2',
-      weight: '800',
-      style: 'normal',
-    },
-    {
-      path: '../asset/fonts/Pretendard-Black.woff2',
-      weight: '900',
       style: 'normal',
     },
   ],
